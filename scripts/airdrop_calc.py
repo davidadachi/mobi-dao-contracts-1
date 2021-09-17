7c5b4cb99408b02c6edb512674e962c36e12be5e
Kyle <kyscott@umich.edu>
2021-09-17 13:19:51 -0400

    update early users

--- scripts/airdrop_calc.py
+++ scripts/airdrop_calc.py
@@ -4,26 +4,34 @@ import json
 filename = 'data.csv'
 sum = 0
 
-data = []
+data = {}
+sum = 0
+filtered_data = {}
 out = {}
 
-with open(filename, 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    fields = next(csvreader)
-    for row in csvreader:
-        amount = float(row[6].replace(',', ''))
-        if amount > 9.5: 
-            sum += amount
-            data.append([row[4], float(row[6].replace(',', ''))])
+for fil in ['usdc.csv', 'usdt.csv']:
+    with open(fil, 'r') as f:
+        csvreader = csv.reader(f)
+        fields = next(csvreader)
+        for row in csvreader:
+            if row[0] in data:
+                data[row[0]] += float(row[1].replace(',', ''))
+            else :
+                data[row[0]] = float(row[1].replace(',', ''))
 
-for d in data:
-    # print(d[0], d[1]/sum)
-    out.update({d[0]: d[1]/sum})
+for key, val in data.items():
+    if val > 9.5:
+        filtered_data[key] = val
+        sum += val
+
+for key, val in filtered_data.items():
+    out.update({key: val/sum})
 
 with open('early-users-mobius.json', 'w+') as f:
     json.dump(out, f)
-    # sum = 0
-    # for num in out.values():
-    #     sum += num
 
-    # print(sum)
+    # check 
+    sum = 0
+    for num in out.values():
+        sum += num
+    print(sum)
