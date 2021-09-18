14ab6b86b2b8596af97bc34ae81984baa6b8e208
Kyle <kyscott@umich.edu>
2021-09-18 16:26:01 -0400

    remove eric from addresses

--- scripts/airdrop_calc.py
+++ scripts/airdrop_calc.py
@@ -9,15 +9,24 @@ sum = 0
 filtered_data = {}
 out = {}
 
+not_in = {
+    '0xBcaA871742Cc603C297e956DF8d6A398ef975551',
+    '0xE35E97438Fd16593e285546260C8585dea7909Dd',
+    '0x9c71787c5b5885e11F74af7E5Dc1324e87375a95',
+    }
+
 for fil in ['usdc.csv', 'usdt.csv']:
     with open(fil, 'r') as f:
         csvreader = csv.reader(f)
         fields = next(csvreader)
         for row in csvreader:
-            if row[0] in data:
-                data[row[0]] += float(row[1].replace(',', ''))
-            else :
-                data[row[0]] = float(row[1].replace(',', ''))
+            if row[0] not in not_in:
+                if row[0] in data:
+                    data[row[0]] += float(row[1].replace(',', ''))
+                else :
+                    data[row[0]] = float(row[1].replace(',', ''))
+            else: 
+                print('eric!')
 
 for key, val in data.items():
     if val > 9.5:
